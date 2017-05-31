0e083c26c0587bcc026c96844f1dfba9f16349a6
Anna Kuznetsova, Esvethlynna Pangelinan and Yokke Hargono <yokke@>
2017-05-31 10:32:31 -0700

    Create a flashcard

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#word1").click(function() {
+    $("#def1").show();
+  });
+
+  $("#def1").click(function() {
+  $("#def1").hide();
+  });
+
+  $("#word2").click(function() {
+    $("#def2").show();
+  });
+
+  $("#def2").click(function() {
+  $("#def2").hide();
+  });
+
+  $("#word3").click(function() {
+    $("#def3").show();
+  });
+
+  $("#def3").click(function() {
+  $("#def3").hide();
+  });
+});
